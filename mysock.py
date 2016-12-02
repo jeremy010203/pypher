2425d00d8c486f026e679f9e060fd2a98552393a
Jérémy Lugand <lugand_j@epita.fr>
2016-12-02 22:50:59 +0100

    refactor: socket get sock function

---
+++ mysock.py
@@ -0,0 +1,21 @@
+def get_socket(ip, po):
+    s = None
+
+    for res in socket.getaddrinfo(ip, po, socket.AF_UNSPEC, socket.SOCK_STREAM):
+        af, socktype, proto, canonname, sa = res
+        try:
+            s = socket.socket(af, socktype, proto)
+        except OSError as msg:
+            s = None
+            continue
+        try:
+            s.connect(sa)
+        except OSError as msg:
+            s.close()
+            s = None
+            continue
+        break
+    if s is None:
+        print('could not open socket')
+        sys.exit(1)
+    return s
