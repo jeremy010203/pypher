04dc7ebf9de820c332613d1d39b36a1c61957a30
Jérémy Lugand <lugand_j@epita.fr>
2016-12-02 22:51:22 +0100

    refactor: socket get sock function

--- client.py
+++ client.py
@@ -1,36 +1,12 @@
-import socket
+import socket, mysock
 import sys
 
 HOST = 'localhost'    # The remote host
 PORT = sys.argv[1]
 
-def get_socket():
-    global HOST
-    global PORT
-    s = None
-
-    for res in socket.getaddrinfo(HOST, PORT, socket.AF_UNSPEC, socket.SOCK_STREAM):
-        af, socktype, proto, canonname, sa = res
-        try:
-            s = socket.socket(af, socktype, proto)
-        except OSError as msg:
-            s = None
-            continue
-        try:
-            s.connect(sa)
-        except OSError as msg:
-            s.close()
-            s = None
-            continue
-        break
-    if s is None:
-        print('could not open socket')
-        sys.exit(1)
-    return s
-
 inp = input('>')
 while inp != 'exit':
-    with get_socket() as s:
+    with mysock.get_socket(HOST, PORT) as s:
         if inp == '':
             s.sendall(b'\r\n')
         else:
--- proxy.py
+++ proxy.py
@@ -1,4 +1,4 @@
-import socketserver, socket
+import socketserver, socket, mysock
 import sys
 import gopher
 
@@ -6,35 +6,13 @@ HOST = 'localhost'
 SERVER_IP = 8000
 PROXY_IP = 8004
 
-def get_socket(ip, po):
-    s = None
-
-    for res in socket.getaddrinfo(ip, po, socket.AF_UNSPEC, socket.SOCK_STREAM):
-        af, socktype, proto, canonname, sa = res
-        try:
-            s = socket.socket(af, socktype, proto)
-        except OSError as msg:
-            s = None
-            continue
-        try:
-            s.connect(sa)
-        except OSError as msg:
-            s.close()
-            s = None
-            continue
-        break
-    if s is None:
-        print('could not open socket')
-        sys.exit(1)
-    return s
-
 class MyTCPHandler(socketserver.BaseRequestHandler):
     def handle(self):
         self.data = self.request.recv(1024)
         print("{} wrote: {}".format(self.client_address[0], self.data))
         rep = self.data.decode()
 
-        with get_socket(HOST, SERVER_IP) as s:
+        with mysock.get_socket(HOST, SERVER_IP) as s:
             print('Sending ' + '.' + rep.split(' ')[1])
             s.sendall(b'.' + rep.split(' ')[1].encode())
             d = s.recv(1024)
